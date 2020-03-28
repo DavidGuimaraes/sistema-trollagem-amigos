652f0eadefab1ce3db2b6460b808156dbcd511e0
David Guimaraes <david.ce@live.com>
2020-03-28 02:41:35 -0400

    02. Inicializando o serviço de descoberta (.gitignore, pom.xml, application.properties e @EnableEurekaServer), além de configurar o serviço sta-contatos como um EurekaClient.

--- sta-contatos/src/main/java/co/davidguimaraes/stacontatos/StaContatosApplication.java
+++ sta-contatos/src/main/java/co/davidguimaraes/stacontatos/StaContatosApplication.java
@@ -2,8 +2,10 @@ package co.davidguimaraes.stacontatos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class StaContatosApplication {
 
 	public static void main(String[] args) {
---
+++ sta-discovery/src/test/java/co/davidguimaraes/stadiscovery/StaDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package co.davidguimaraes.stadiscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StaDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
